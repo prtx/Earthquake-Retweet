578ebbf96951e3aa7ebc75459cc74ae7a602fa0d
prtx <prtx.pratikshrestha@gmail.com>
2017-09-15 10:09:08 +0545

    Cleanup

--- engine.py
+++ engine.py
@@ -1,83 +1,112 @@
-import tweepy,json,sys,time
+import tweepy
+import json
+import sys
+import time
 from keys import all_keys
 
 # -*- coding: utf-8 -*-
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-include = ['ambulance','volunteer','need','relief','contact','emergency','food','water','blood','tent','hospital']
-exclude = ['donate','~rt ','gospel','pray','?']
+include = [
+    'ambulance',
+    'volunteer',
+    'need',
+    'relief',
+    'contact',
+    'emergency',
+    'food',
+    'water',
+    'blood',
+    'tent',
+    'hospital'
+]
+
+exclude = [
+    'donate',
+    '~rt ',
+    'gospel',
+    'pray',
+    '?'
+]
+
 places = open('places.txt').read().split()
 
 
-def list_on_tweet(array,tweet):
-	
-	for word in array:
-		if word in tweet:
-			return True
-	
+def list_on_tweet(array, tweet):
+    
+    for word in array:
+        if word in tweet:
+            return True
+    
 
 class StdOutListener(tweepy.StreamListener):
-	
-	i=0
-	check_time = time.time()
-	posted_tweets = []
-	
-	def on_data(self, data):
-	
-		csv = open('tweets.csv','a')
-		self.i+=1
-		tweet = json.loads(data)
-		print tweet['text']
-		csv.write(
-			str(self.i)+'\t'+																#S.no
-			str(tweet['id'])+'\t'+														#tweet id
-			tweet['user']['id_str']+'\t'+												#user id
-			tweet['user']['name'].replace('\t',' ').replace('\n',' ')+'\t'+				#name
-			tweet['user']['location'].replace('\t',' ').replace('\n',' ')+'\t'+			#location
-			tweet['text'].replace('\t',' ').replace('\n',' ')+'\t'+						#tweet
-			tweet['created_at']+'\t'+													#datetime
-			str(tweet['favorite_count'])+'\t'+											#favorite count
-			str(tweet['retweet_count'])+'\n'											#RT count
-		)
-		
-		if list_on_tweet(include,tweet['text'].lower()) and not list_on_tweet(exclude,'~'+tweet['text'].lower()) and tweet['text'] not in self.posted_tweets:
-			_api.retweet(id=tweet['id'])
-			try:
-				self.posted_tweets.append(tweet['text'])
-				print 'retweeted'
-			except Exception:
-				pass
-			
-		
-		if self.i%18000==0:
-			while True:
-				print time.time() - check_time
-				if time.time() - check_time > 900:
-					break
-		
-		return True
+    
+    i = 0
+    check_time = time.time()
+    posted_tweets = []
+    
+    def on_data(self, data):
+    
+        csv = open('tweets.csv', 'a')
+        self.i += 1
+        tweet = json.loads(data)
+        print tweet['text']
+        csv.write(
+            str(self.i)+'\t'+                                                   #S.no
+            str(tweet['id'])+'\t'+                                              #tweet id
+            tweet['user']['id_str']+'\t'+                                       #user id
+            tweet['user']['name'].replace('\t',' ').replace('\n',' ')+'\t'+     #name
+            tweet['user']['location'].replace('\t',' ').replace('\n',' ')+'\t'+ #location
+            tweet['text'].replace('\t',' ').replace('\n',' ')+'\t'+             #tweet
+            tweet['created_at']+'\t'+                                           #datetime
+            str(tweet['favorite_count'])+'\t'+                                  #favorite count
+            str(tweet['retweet_count'])+'\n'                                    #RT count
+        )
+        
+        tweet_to_include = list_on_tweet(include, tweet['text'].lower())
+        tweet_to_exclude = list_on_tweet(exclude, '~'+tweet['text'].lower())
+        new_tweet = tweet['text'] not in self.posted_tweets
 
-	def on_error(self, status):
-		print status
-
-
-
-if __name__ == '__main__':	
-	key = all_keys[0]
-	tweetslist = []
-	l = StdOutListener()
-	auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
-	auth.set_access_token(key.access_token, key.access_token_secret)
-	api = tweepy.API(auth)
-	
-	key = all_keys[1]
-	
-	_auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
-	_auth.set_access_token(key.access_token, key.access_token_secret)
-	_api = tweepy.API(_auth)
-
-	stream = tweepy.Stream(auth, l)
-	stream.filter(track=['#nepalquakerelief','#earthquakenepal','earthquake'],async = True)
+        if tweet_to_include and not tweet_to_exclude and new_tweet:
+            _api.retweet(id=tweet['id'])
+            try:
+                self.posted_tweets.append(tweet['text'])
+                print 'retweeted'
+            except Exception:
+                pass
+            
+        
+        if self.i%18000 == 0:
+            while True:
+                print time.time() - check_time
+                if time.time() - check_time > 900:
+                    break
+        return True
 
 
+    def on_error(self, status):
+        print status
+
+
+def tracker():
+
+    key = all_keys[0]
+    tweetslist = []
+    l = StdOutListener()
+    auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+    auth.set_access_token(key.access_token, key.access_token_secret)
+    api = tweepy.API(auth)
+    
+    key = all_keys[1]
+    
+    _auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+    _auth.set_access_token(key.access_token, key.access_token_secret)
+    _api = tweepy.API(_auth)
+
+    stream = tweepy.Stream(auth, l)
+    stream.filter(track=['#nepalquakerelief', '#earthquakenepal', 'earthquake'], async = True)
+
+
+if __name__ == '__main__':    
+    tracker()
